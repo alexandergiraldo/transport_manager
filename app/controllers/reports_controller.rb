899f2adb33a76e2fb09780d9904d261a9d146b4e
Alexander Giraldo <algirald@cisco.com>
2023-05-16 16:42:54 -0500

    Add new charts to the reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,5 +3,7 @@ class ReportsController < ApplicationController
     reports = Report.new
     @vehicle_utilities_by_year = reports.vehicle_utilities_by_year(current_vehicle.id, params[:year])
     @vehicle_utilities_by_month = reports.vehicle_utilities_by_month(current_vehicle.id, params[:year])
+    @total_vehicle_utilities = reports.total_vehicle_utilities(current_vehicle.id)
+    @total_vehicle_utilities_by_month = reports.total_vehicle_utilities_by_month(current_vehicle.id, params[:year])
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -32,4 +32,25 @@ class Report
       ]
     }
   end
+
+  def total_vehicle_utilities_by_month(vehicle_id, year = null)
+    year = Time.zone.now.year unless year
+    Time.use_zone("UTC") {
+      start_date = Date.new(year.to_i,1,1)
+      end_date = Date.new(year.to_i,12,31)
+      income = Register.incoming.where(vehicle_id: vehicle_id).group_by_month(:event_date, format: "%b", range: start_date..end_date).sum(:value)
+      outcome = Register.outcoming.where(vehicle_id: vehicle_id).group_by_month(:event_date, format: "%b", range: start_date..end_date).sum(:value)
+      return [
+        {
+          "name" => "Total utilidades por mes",
+          "data" => income.reduce({}) { |h, (k, v)| h[k] = v - outcome[k]; h }
+        },
+      ]
+    }
+  end
+
+  def total_vehicle_utilities(vehicle_id)
+    data = Register.select("value, register_type").where(vehicle_id: vehicle_id).group(:register_type).sum(:value)
+    return data["incoming"] - data["outcoming"]
+  end
 end

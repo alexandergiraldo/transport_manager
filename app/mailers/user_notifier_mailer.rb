f957296e7e7f89a6214fe70039b1d8b210f2cf93
Alexander Giraldo <algirald@cisco.com>
2024-08-14 09:54:56 -0500

    Fix mailer issues

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -3,6 +3,7 @@ class UserNotifierMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that contains the user's email address
   def send_signup_email(user)
     @user = user
+    @plates = Vehicle.where(id: user.all_vehicle_ids).pluck(:license_plate)
     mail(
       :to => @user.email,
       :subject => 'Bienvenido a Transport Manager' )
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: ENV["SITE_DOMAIN"], protocol: :https }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 

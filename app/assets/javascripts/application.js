04947d72298cef9a1fb44cc44c3e1e90f4b3ce19
Alexander Giraldo <alexgr200@gmail.com>
2024-11-29 15:00:40 -0500

    Fix js issue on sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -112,15 +112,17 @@ $('.dropdown.notifications ul a.nav-link').click(function (e) {
 // Self Initialize DOM Factory Components
 domFactory.handler.autoInit();
 
-// Connect button(s) to drawer(s)
-var sidebarToggle = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="sidebar"]'));
+document.addEventListener('DOMContentLoaded', function () {
+  // Connect button(s) to drawer(s)
+  var sidebarToggle = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="sidebar"]'));
 
-sidebarToggle.forEach(function (toggle) {
-  toggle.addEventListener('click', function (e) {
-    var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer';
-    var drawer = document.querySelector(selector);
-    if (drawer) {
-      drawer.mdkDrawer.toggle()
-    }
+  sidebarToggle.forEach(function (toggle) {
+    toggle.addEventListener('click', function (e) {
+      var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer';
+      var drawer = document.querySelector(selector);
+      if (drawer) {
+        drawer.mdkDrawer.toggle()
+      }
+    })
   })
-})
+});

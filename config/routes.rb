83481efc7ef34a6b43ae192d9d055b268f3b32e7
Alexander Giraldo <alexgr200@gmail.com>
2024-12-05 13:38:30 -0500

    Redirect herokuapp to site domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  constraints(host: /.herokuapp/) do
+    match '/(*path)' => redirect { |params, req|
+      "https://#{ENV["SITE_DOMAIN"]}#{req.fullpath}"
+    }, via: [:get, :head]
+  end
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 

8c47ac3df2b5a9bcb162355df02ce85c61ece3e8
Alex Giraldo <alexander.giraldo.ctr@gocaribou.com>
2024-12-23 15:10:02 -0500

    Rails 7 upgrade (#7)

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,5 +5,5 @@ class Payment < ApplicationRecord
 
   validates :payment_date, :amount, :payment_method, presence: true
 
-  enum payment_method: { bank_transfer: 0, cash: 1, card: 2, check: 3, other: 4 }
+  enum :payment_method, { bank_transfer: 0, cash: 1, card: 2, check: 3, other: 4 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [ :account_user, :account_admin, :super_admin ]
+  enum :role, [ :account_user, :account_admin, :super_admin ]
 
   # Associations
   has_many :account_users, class_name: "AccountUser"

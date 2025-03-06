198f5638e5ba19037a83ce911d21ba3e61bed741
Alexander Giraldo <alexgr200@gmail.com>
2025-03-06 17:56:59 -0500

    chore: Update Rails configuration defaults to version 8.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module TaxiManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,9 +23,6 @@ module TaxiManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Preserve timezone when converting to time
-    config.active_support.to_time_preserves_timezone = :zone
 
     # Handle exceptions
     config.exceptions_app = self.routes
